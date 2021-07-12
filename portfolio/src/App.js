0d7af092ea2022b763568ff3a788b429f61d1642
bdurham227 <bdurham227@gmail.com>
2021-07-12 16:46:41 -0400

    set up layout of how i want things to render in app.js and added some stylings

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,19 @@
-
+import Topbar from "./components/Topbar/Topbar";
+import Intro from "./components/Intro/intro";
+import Portfolio from "./components/Portfolio/portfolio";
+import Work from "./components/Work/works";
+import Contact from "./components/Contact/contact";
 
 function App() {
   return (
    <div className="app">
      <Topbar/>
-     top
+     <div className="sections">
+       <Intro/>
+       <Portfolio/>
+       <Work/>
+       <Contact/>
+     </div>
      section - intro - portfoliopage - work ...with projects
    </div>
   );
